1f338cdab49a706ec08212bbc0d8062024193bd4
github0null <me@github0null.io>
2023-03-15 17:42:49 +0800

    update cmakelist

--- src/main.c
+++ src/main.c
@@ -17,6 +17,16 @@ void UserMain(void)
     log_i(" - apb    freq: %d MHz", sys_clk.apbclk);
     log_i(" - wlan   freq: %d MHz", sys_clk.wlanclk);
 
+    u8 mac[6];
+
+    if (tls_get_bt_mac_addr(mac) == TLS_EFUSE_STATUS_OK) {
+        log_i(" - ble     mac: %02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    }
+
+    if (tls_get_mac_addr(mac) == TLS_EFUSE_STATUS_OK) {
+        log_i(" - wifi    mac: %02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    }
+
     log_i("------------------------------------");
 
     while (true) {
